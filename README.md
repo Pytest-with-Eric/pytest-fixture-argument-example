c86dfdafa2aea8a37bc91c6d30492595f255da88
ericsalesdeandrade <sdaeric19@gmail.com>
2023-09-05 09:32:26 +0530

    Add test code

---
+++ README.md
@@ -0,0 +1,22 @@
+# Pytest Fixture With Argument Example
+
+<!-- This repo contains the sample code for the article - [What Are Pytest Fixture Scopes? (How To Choose The Best Scope For Your Test)](https://pytest-with-eric.com/pytest-advanced/pytest-fixture-scope/)
+
+This project is a simple example of what are Pytest Fixtures scopes and how to use them. -->
+
+# Requirements
+* Python 3.11.4
+
+Please install the dependencies via the `requirements.txt` file using 
+```commandline
+pip install -r requirements.txt
+```
+If you don't have Pip installed please follow instructions online on how to do it.
+
+# How To Run the Unit Tests
+To run the Unit Tests, from the root of the repo run
+```commandline
+pytest -v -s
+```
+
+If you have any questions about the project please raise an Issue on GitHub. 
---
+++ tests/test_basic_example.py
@@ -0,0 +1,48 @@
+import pytest
+
+
+@pytest.fixture
+def simple_data():
+    return 42
+
+
+def test_simple_data(simple_data):
+    assert simple_data == 42
+
+
+# Define a fixture with parameters
+@pytest.fixture(params=[0, 1, 2])
+def param_data(request):
+    return request.param
+
+
+def test_param_data(param_data):
+    assert param_data in [0, 1, 2]
+
+
+# Define a fixture that takes an argument
+@pytest.fixture
+def square(request):
+    return request.param * 2
+
+
+# Use indirect parametrization to pass arguments to the fixture
+@pytest.mark.parametrize("square", [1, 2, 3], indirect=True)
+def test_square(square):
+    assert square in [2, 4, 6]
+
+
+# Use Factories as fixture
+@pytest.fixture
+def user_creds():
+    def _user_creds(name: str, email: str):
+        return {"name": name, "email": email}
+
+    return _user_creds
+
+
+def test_user_creds(user_creds):
+    assert user_creds("John", "abc@xyz.com") == {
+        "name": "John",
+        "email": "abc@xyz.com",
+    }
